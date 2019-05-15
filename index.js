a165c648faca6e8023df95c25a30f719c27e3b84
Aingaran <eaingaran@gmail.com>
2019-05-15 13:27:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+	document.body.className = '';
+};
+window.ontouchmove = function() {
+	return false;
+};
+window.onorientationchange = function() {
+	document.body.scrollTop = 0;
+};
