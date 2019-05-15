13bf9bf7af1912aa6e52561f30b1620a901c5d4c
Aingaran <me@aingaran.dev>
2019-05-15 21:49:36 +0530

    Revert "Update index.js"
    This reverts commit d9a6fcc2b3de3a53274f6c0c86f4af8064d26e3a.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function delayLoadPage()  {
   setTimeout("", 2000);
-  document.getElementById("home").style.display = 'none';
+  document.getElementById("main").style.display = 'none';
   document.getElementById("ready").style.display = 'block';
 };
 window.onload = function() {
